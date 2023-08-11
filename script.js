3ecf74ac3843d9d74ec9326d32636760f1737d18
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2023-08-11 03:13:38 +0100

    document files added

---
+++ script.js
@@ -0,0 +1,11 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var Color2 = document.querySelector(".color2");
+
+console.log(css);
+console.log(Color2);
+console.log(color1);
+
+
+
+
