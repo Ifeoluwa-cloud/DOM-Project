c6304770a1e9ba0a4e3b31764cbc23d7d9319b2f
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2023-08-14 14:53:28 +0100

    Javascript SetGradient function modified

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ var body = document.getElementById("gradient");
 
 function setGradient(){
     body.style.background = 
-        "linear-gradient(to right, " 
-        + color1.value 
-        + ", "
-        + color2.value 
-        + ")";
-      
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", "
+    + color2.value 
+    + ")";
+
+    css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient)
